53957dfc83ea605507d4a2e9cc0046261971a92f
S NAVEEN KUMAR <navee1110@gmail.com>
2024-02-03 18:06:54 +0530

    user model and user route created

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const { connection } = require("./db")
+const { userRouter } = require("./routes/user.routes")
+app.use(express.json())
+app.use(cors())
+app.use("/", userRouter)
+
+require('dotenv').config()
+const port = process.env.PORT
+app.listen(port, async () => {
+
+    try {
+        await connection
+        console.log("connected to db")
+        console.log(`server running in port ${port}`)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
