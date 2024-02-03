631b3a364a45d311ee51de68ec9b12baac6ceaab
S NAVEEN KUMAR <navee1110@gmail.com>
2024-02-03 19:17:41 +0530

    book route updated

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -4,7 +4,7 @@ const { auth } = require("../middlewares/auth.middleware");
 
 const bookRouter = express.Router();
 bookRouter.use(auth)
-bookRouter.post("/add", async (req, res) => {
+bookRouter.post("/", async (req, res) => {
     try {
         const newBook = new BookModel(req.body)
         await newBook.save()
@@ -23,7 +23,7 @@ bookRouter.get("/", async (req, res) => {
         res.status(400).send({ "err": err })
     }
 })
-bookRouter.patch("/update/:id", async (req, res) => {
+bookRouter.patch("/:id", async (req, res) => {
     const { id } = req.params;
     const book = await BookModel.findOne({ _id: id })
     console.log("book", book)
@@ -41,7 +41,7 @@ bookRouter.patch("/update/:id", async (req, res) => {
 
     }
 })
-bookRouter.delete("/delete/:id", async (req, res) => {
+bookRouter.delete("/:id", async (req, res) => {
     const { id } = req.params;
     const book = await BookModel.findOne({ _id: id })
     console.log("book", book)
@@ -59,5 +59,26 @@ bookRouter.delete("/delete/:id", async (req, res) => {
 
     }
 })
-
+bookRouter.get("/old", async (req, res) => {
+    try {
+        const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
+        console.log(tenMinutesAgo)
+        const oldBooks = await BookModel.find({ createdAt: { $lt: tenMinutesAgo } });
+        res.status(200).send(oldBooks)
+    }
+    catch (err) {
+        res.status(400).send({ "err": err })
+    }
+})
+bookRouter.get("/new", async (req, res) => {
+    try {
+        const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
+        console.log(tenMinutesAgo)
+        const newBooks = await BookModel.find({ createdAt: { $gte: tenMinutesAgo } });
+        res.status(200).send(newBooks)
+    }
+    catch (err) {
+        res.status(400).send({ "err": err })
+    }
+})
 module.exports = { bookRouter }
